c216e561b2210fbc126503d1e044241fae072e9e
Max Schmidt <to.maxschmidt@pm.me>
2024-12-02 15:36:50 +0100

    add part1 day2

--- d2/m1.py
+++ d2/m1.py
@@ -1,9 +1,15 @@
 ls = []
+n_safe = 0
 
 with open("d2/d2") as file:
     for line in file:
         levels = [int(x) for x in line.split(' ')]
-        diffs = [levels[i + 1] - x for i, x in enumerate(levels[:-1])]
-        ls.append(diffs)
+        ls.append([levels[i + 1] - x for i, x in enumerate(levels[:-1])])
 
 for l in ls:
+    abs_diffs = [abs(x) for x in l]
+    is_in_bounds = max(abs_diffs) <= 3 and min(abs_diffs) > 0
+    is_monotone = all([x >0 for x in l]) or all(x < 0 for x in l)
+    n_safe += 1 if is_in_bounds and is_monotone else 0 
+
+print(n_safe)
